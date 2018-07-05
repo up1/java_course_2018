6ec0257c9581b2c1568de12ea7a9ec06d5857fbe
somkiat <somkiat.p@gmail.com>
2018-07-05 10:18:45 +0700

    Workshop List with sorting

---
+++ src/main/java/day4/MainSorting.java
@@ -0,0 +1,35 @@
+package day4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MainSorting {
+
+    public static void main(String[] args) {
+        MainSorting p = new MainSorting();
+        p.start();
+    }
+
+    private void start() {
+        // Create data
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, "Somkiat", 200));
+        employees.add(new Employee(2, "Pui", 100));
+        employees.add(new Employee(3, "Abdul", 500));
+        employees.add(new Employee(4, "Up1", 150));
+        employees.forEach(System.out::println);
+        
+        // Sorting with Comparator
+        Collections.sort(employees, new SortingBySalaryWithDecending());
+        employees.forEach(System.out::println);
+    }
+}
+
+class SortingBySalaryWithDecending implements Comparator<Employee> {
+    @Override
+    public int compare(Employee e1, Employee e2) {
+        return (int)(e2.getSalary() - e1.getSalary());
+    }
+}
---
+++ src/main/java/demo/java8/DemoGeneric.java
@@ -0,0 +1,7 @@
+package demo.java8;
+
+public class DemoGeneric {
+
+}
+
+
